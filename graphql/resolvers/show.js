035cf1f8811eb27977094e61868284d0b8f30b0f
prabhuomkar <prabhuomkar@yandex.com>
2019-06-06 22:38:51 +0530

    API endpoint fixed

--- graphql/resolvers/show.js
+++ graphql/resolvers/show.js
@@ -5,14 +5,14 @@ const { transformShow } = require('./merge')
 module.exports = {
     shows: async (args) => {
         try {
-            args.orderBy = args.orderBy ? args.OrderBy : { '_id': 1 }
+            args.orderBy = args.orderBy ? args.orderBy : { '_id': 1 }
             args.where = args.where ? args.where : {}
             let episodeArgs = {}
-            episodeArgs.orderBy = args.orderBy.episodes ? args.orderBy.episodes : {}
-            episodeArgs.where = args.where.episodes ? args.where.episodes : {}
-            delete args.orderBy.episodes
-            delete args.where.episodes
-            const shows = await Show.find().limit(args.limit).skip(args.offset).sort(args.orderBy).where(args.where)
+            episodeArgs.orderBy = args.orderBy ? (args.orderBy.episodes ? args.orderBy.episodes : {}) : {}
+            episodeArgs.where = args.where ? (args.where.episodes ? args.where.episodes : {}) : {}
+            if (args.orderBy) delete args.orderBy.episodes
+            if (args.where) delete args.where.episodes
+            const shows = await Show.find().where(args.where).sort(args.orderBy).limit(args.limit).skip(args.offset)
             return shows.map(show => {
                 return transformShow(show, episodeArgs)
             })
