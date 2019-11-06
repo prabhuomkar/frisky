0c6adb0869f81d1fe3b528c64a85aa8229f67840
Swap76 <swapnil.satish17@siesgst.ac.in>
2019-11-06 16:28:34 +0530

    Improved Where Clause for Service Providers

--- graphql/resolvers/search.js
+++ graphql/resolvers/search.js
@@ -7,12 +7,13 @@ module.exports = {
 		try {
 			args.orderBy = args.orderBy ? args.orderBy : { '_id': 1 }
 			args.where = args.where ? args.where : {}
+			if (args.where.serviceProviders) args.where.serviceProviders = JSON.parse('{ "$in": '+ JSON.stringify(args.where.serviceProviders) + '}')
 			let episodeArgs = {}
 			episodeArgs.orderBy = args.orderBy ? (args.orderBy.episodes ? args.orderBy.episodes : {}) : {}
 			episodeArgs.where = args.where ? (args.where.episodes ? args.where.episodes : {}) : {}
 			if (args.orderBy) delete args.orderBy.episodes
 			if (args.where) delete args.where.episodes
-			const shows = await Show.find({ $text: { $search: args.query } }).skip(args.offset).sort(args.orderBy).limit(args.limit)
+			const shows = await Show.find({ $text: { $search: args.query } }).where(args.where).skip(args.offset).sort(args.orderBy).limit(args.limit)
 			return shows.map(show => {
 				return transformShow(show, episodeArgs)
 			})
